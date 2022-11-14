e4bb7a5ccc8cc2a8d642ad6115030dd29ce7ab45
Heba Elsaid <hebaelsaid912@gmail.com>
2022-11-14 16:26:17 +0200

    handle splash screen design and logic

--- app/src/main/java/com/hebaelsaid/android/newsapp_kotlin/ui/feature/splash/SplashFragment.kt
+++ app/src/main/java/com/hebaelsaid/android/newsapp_kotlin/ui/feature/splash/SplashFragment.kt
@@ -1,10 +1,13 @@
 package com.hebaelsaid.android.newsapp_kotlin.ui.feature.splash
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.hebaelsaid.android.newsapp_kotlin.databinding.FragmentSplashBinding
 
 private const val TAG = "SplashFragment"
@@ -18,6 +21,13 @@ class SplashFragment : Fragment() {
         // Inflate the layout for this fragment
         binding =  FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+             findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment())
+        }, 10000)
     }
 
 }
